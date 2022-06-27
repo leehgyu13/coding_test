186def0c159ea6b76ebbc54ea576eacfa9442c09
Honggyu <6398lhg@gmail.com>
2022-06-28 03:34:27 +0900

    2021 카카오 숫자 문자열(다른 풀이 추가)

--- 2021_kakao_strToNum.py
+++ 2021_kakao_strToNum.py
@@ -13,3 +13,14 @@ def solution(s):
     answer = s
     
     return answer
+'''
+다른 사람 풀이
+num_dic = {"zero":'0', "one":'1', "two":'2', "three":'3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
+
+def solution(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+
+    return int(answer)
+'''
