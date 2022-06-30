bfda1f2252db2ea47cc77f7e1fcad5cb4ec071ee
Honggyu08 <6398lhg@gmail.com>
2022-06-30 13:59:39 +0900

    Summer/Winter Coding (~2018) add other sol

--- 2018_summer_winder_coding_make_prime.py
+++ 2018_summer_winder_coding_make_prime.py
@@ -21,3 +21,16 @@ def solution(nums):
                     prime = False
 
     return answer
+
+'''
+from itertools import combinations
+def prime_number(x):
+    answer = 0
+    for i in range(1,int(x**0.5)+1):
+        if x%i==0:
+            answer+=1
+    return 1 if answer==1 else 0
+
+def solution(nums):
+    return sum([prime_number(sum(c)) for c in combinations(nums,3)])
+'''
