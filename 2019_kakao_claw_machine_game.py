4aad51eeac9cc239d469ad64d43ae1de9a986b0b
Honggyu08 <6398lhg@gmail.com>
2022-07-03 22:52:20 +0900

    2019 kakao winter internship claw machine game(add other sol)

--- 2019_kakao_claw_machine_game.py
+++ 2019_kakao_claw_machine_game.py
@@ -19,3 +19,24 @@ def solution(board, moves):
                 break
     
     return answer
+
+    '''
+    def solution(board, moves):
+        stacklist = []
+        answer = 0
+
+        for i in moves:
+            for j in range(len(board)):
+                if board[j][i - 1] != 0:
+                    stacklist.appen(board[j][i - 1])
+                    board[j][i - 1] = 0
+
+                    if len(stacklist) > 1:
+                        if stacklist[-1] == stacklist[-2]:
+                            stacklist.pop(-1)
+                            stacklist.pop(-1)
+                            answer += 2
+                    break
+                
+        return answer
+    '''
