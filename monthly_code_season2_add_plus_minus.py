828278894c144e949614b8e00964b0dcbbfba2a5
Honggyu08 <6398lhg@gmail.com>
2022-07-05 22:13:41 +0900

    월간 코드 챌린지 시즌2 음양 더하기

---
+++ monthly_code_season2_add_plus_minus.py
@@ -0,0 +1,14 @@
+def applSign(num, sign):
+    if sign:
+        return num
+    elif not(sign):
+        return -num
+
+def solution(absolutes, signs):
+    answer = 123456789
+    
+    for i in range(len(absolutes)):
+        absolutes[i] = applSign(absolutes[i], signs[i])
+    
+    answer = sum(absolutes)
+    return answer
