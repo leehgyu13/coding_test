b3a8dbaf037f64e1bf896817072ea5318eaf02d8
Honggyu08 <6398lhg@gmail.com>
2022-07-06 21:29:01 +0900

    월간 코드 챌린지 시즌1 내적(add other's sol)

--- monthly_code_season1_inner_product.py
+++ monthly_code_season1_inner_product.py
@@ -5,3 +5,10 @@ def solution(a, b):
     answer = sum(list(map(mult, zip(a,b))))
     
     return answer
+
+'''
+other's sol
+
+def solution(a, b):
+    return sum([x * y for x, y in zip(a, b)])
+'''
