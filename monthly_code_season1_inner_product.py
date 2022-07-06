a26fea464b1795802a8c2f00c309757fe53ea116
Honggyu08 <6398lhg@gmail.com>
2022-07-06 21:33:39 +0900

    월간 코드 챌린지 시즌1 내적(add my another sol)

--- monthly_code_season1_inner_product.py
+++ monthly_code_season1_inner_product.py
@@ -7,10 +7,22 @@ def solution(a, b):
     return answer
 
 '''
+another sol
+
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += a[i] * b[i]
+        
+    return answer
+'''
+'''
 other's sol
 
 def solution(a, b):
     return sum([x * y for x, y in zip(a, b)])
-
+'''
+'''
+other's sol2
 solution = lambda x, y: sum(a*b for a, b in zip(x,y))
 '''
